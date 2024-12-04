db64468ccc833b66d87b31047495ac6a181cbeb1
Sven Oliver Berger <bergersvenoliver@gmail.com>
2024-12-04 07:42:55 +0100

    main: final release

--- lf08/includes/database.php
+++ lf08/includes/database.php
@@ -1,9 +1,9 @@
 <?php
 // Datenbank-Zugangsdaten
 $server = "localhost";
-$benutzer = "gfn_system";
+$benutzer = "gfn_fahrradvermietung";
 $passwort = "-c3?8HvD";
-$dbname = "gfn_system";
+$dbname = "gfn_fahrradvermietung";
 
 // Verbindung herstellen
 $verbindung = new mysqli($server, $benutzer, $passwort, $dbname);
