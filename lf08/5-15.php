c72ac1398e477191167d227c2275c9c2c715d748
Sven Oliver Berger <bergersvenoliver@gmail.com>
2024-12-04 09:41:05 +0100

    Create 5-15.php

---
+++ lf08/5-15.php
@@ -0,0 +1,30 @@
+<?php include("includes/header.php"); ?>
+
+SELECT hersteller.herstellernr, hersteller.herstellername, fahrradarten.bezeichnung FROM fahrraeder JOIN fahrradarten, hersteller ORDER BY `hersteller`.`herstellernr` ASC;
+
+<?php include("include/middle.php");?>
+
+<?php
+$sql = "SELECT hersteller.herstellernr, hersteller.herstellername, fahrradarten.bezeichnung FROM fahrraeder JOIN fahrradarten, hersteller ORDER BY `hersteller`.`herstellernr` ASC;";
+$ausgabe = $verbindung->query($sql);
+?>
+<table>
+    <thead>
+        <tr>
+            <th>Fahrradnummer</th>
+            <th>Anschaffungswert</th>
+            <th>Bezeichnung</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php while ($inhalt = $ausgabe->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $inhalt['herstellernr']; ?></td>
+                <td><?php echo $inhalt['herstellername'] . "€"; ?></td>
+                <td><?php echo $inhalt['bezeichnung']; ?></td>
+            </tr>
+        <?php endwhile; ?>
+    </tbody>
+</table>
+
+<?php include("includes/footer.php");
