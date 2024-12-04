0f4a5d1d6bfa821c282e0757e36489d2ed459337
Sven Oliver Berger <bergersvenoliver@gmail.com>
2024-12-04 07:34:01 +0100

    main: correct the userdata

--- lf08/includes/database.php
+++ lf08/includes/database.php
@@ -1,9 +1,9 @@
 <?php
 // Datenbank-Zugangsdaten
 $server = "localhost";
-$benutzer = "sven";
-$passwort = "diesims2";
-$dbname = "fahrradvermietung_2013";
+$benutzer = "gfn_ftp1";
+$passwort = "-c3?8HvD";
+$dbname = "gfn_system";
 
 // Verbindung herstellen
 $verbindung = new mysqli($server, $benutzer, $passwort, $dbname);
