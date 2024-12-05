1f7208abb8ae8b1023a57d43d82206eb7028e0d5
Sven Oliver Berger <bergersvenoliver@gmail.com>
2024-12-05 12:48:03 +0100

    add 5-18

--- lf08/includes/seitenleiste.php
+++ lf08/includes/seitenleiste.php
@@ -18,5 +18,6 @@
         <li><a href="5-15.php">Aufgabe 5.15</a></li>
         <li><a href="5-16.php">Aufgabe 5.16</a></li>
         <li><a href="5-17.php">Aufgabe 5.17</a></li>
+        <li><a href="5-18.php">Aufgabe 5.18</a></li>
     </ul>  
 </div>
